f930df945e36a6dd97d1ac04880d7e84880a93e0
gunnsamagna <102679533+gunnsamagna@users.noreply.github.com>
2022-11-18 10:45:14 +0000

    Update Verk3.py

--- kodar/Verk3.py
+++ kodar/Verk3.py
@@ -1,70 +1,125 @@
 #!/usr/bin/python -u
-
-#!/usr/bin/python -u
-import RPi.GPIO as GPIO
-import time
-import datetime
+import digitalio
 import board
+import time
+from board import *
+import glob
 import adafruit_dht
+import RPi.GPIO as GPIO
 import os
+import datetime
 
-RELAY_FAN_GPIO_PIN = 26
+## A - LIÐUR
+## Kveikjum á viftu
+
+# Configuration
 FAN_GPIO_PIN = 20
-IR_GPIO_PIN = 6
-PWM_FREQ = 125
-PWM_OFF = 0
-PWM_MAX = 100
-TACH_GPIO_PIN = 21
-count_time = 2
+RELAY_FAN_GPIO_PIN = 26 # BCM pin used to turn RELAY for FAN ON/OFF
+RELAY_HEATER_FAN_GPIO_PIN = 16
+##PWM_duty_cycle = 10
+PWM_FREQ = 125        # [Hz] 25kHz for PWM control
 
-def setFanSpeed(PWM_duty_cycle):
-  fan.start(PWM_duty_cycle)
-  return()
 
-def tach_count(sec,GPIO_PIN):
-  duration = datetime.timedelta(seconds=sec)
-  end_time = (datetime.datetime.now()+duration)
-  current_pin_status = GPIO.input(GPIO_PIN)
-  last_pin_status = 2
-  counter = 0
-  while ( datetime.datetime.now() < end_time):
-    current_pin_status=GPIO.input(GPIO_PIN)
-    if (current_pin_status != last_pin_status):
-     # print(current_pin_status)
-      last_pin_status = current_pin_status
-      counter += 1
-  return (counter)
+PWM_OFF = 0             # the PWM_duty_cycle 0%
+PWM_MAX = 100           # the PWM_duty_cycle 100%
+dutycyclegildi = [10, 90]
 
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/temperature'
+
+################################################
 try:
-  print("PWM_FREQ = ",PWM_FREQ)
-  GPIO.setwarnings(False)
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setup(RELAY_FAN_GPIO_PIN, GPIO.OUT, initial=GPIO.HIGH)
-  GPIO.setup(FAN_GPIO_PIN, GPIO.OUT, initial=GPIO.HIGH) # var low
-  GPIO.setup(TACH_GPIO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-  GPIO.setup(IR_GPIO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-  fan = GPIO.PWM(FAN_GPIO_PIN,PWM_FREQ)
-  setFanSpeed(PWM_OFF)
+	GPIO.setwarnings(False)
+	GPIO.setmode(GPIO.BCM)
+
+# PWM used to control the fan's speed
+	GPIO.setup(RELAY_FAN_GPIO_PIN, GPIO.OUT, initial=GPIO.HIGH)
+# tachometer - from a hall sensor located in the fan
+	GPIO.setup(RELAY_HEATER_FAN_GPIO_PIN, GPIO.OUT, initial=GPIO.HIGH)
+
+	GPIO.output(RELAY_FAN_GPIO_PIN,GPIO.LOW) # Turn the FAN ON
+	print("FAN IS ON")
+	time.sleep(5)
+
+	GPIO.output(RELAY_HEATER_FAN_GPIO_PIN, GPIO.LOW)
+	print("HEATER ON")
+except KeyboardInterrupt:
+		GPIO.cleanup() # resets all GPIO ports used by this function
 
 
-  GPIO.output(RELAY_FAN_GPIO_PIN,GPIO.LOW)
-  counter2= 0
-  while (counter2 < 5):
-    duty_cycle = PWM_MAX*counter2/4
-    setFanSpeed(duty_cycle)
-    time.sleep(4)
-    print("Duty cycle:: {:.2f}".format(duty_cycle))
-    tach_hall_rpm = float(tach_count(count_time,TACH_GPIO_PIN))/count_time/2/2*60
-    print("Hall RPM:: {:.2f}".format(tach_hall_rpm))
-    tach_IR_rpm = float(tach_count(count_time, IR_GPIO_PIN))/count_time/2/9*60
-    print("IR RPM:: {:.2f}".format(tach_IR_rpm))
-    print("hlutfall : ", (tach_hall_rpm/tach_IR_rpm)*100)
-    counter2 += 1
-  GPIO.output(RELAY_FAN_GPIO_PIN,GPIO.HIGH)
+# Set fan speed
+def setFanSpeed(PWM_duty_cycle):
+	fan.start(PWM_duty_cycle)
+	return()
+
+def read_temp_raw():
+	f = open(device_file, 'r')
+	line = f.read()
+	f.close()
+	return line
+
+##################################
+try:
+	GPIO.setwarnings(False)
+	GPIO.setup(FAN_GPIO_PIN,GPIO.OUT, initial = GPIO.LOW)
+	fan = GPIO.PWM(FAN_GPIO_PIN, PWM_FREQ)
+	setFanSpeed(PWM_OFF)
+
+#### Setjum Duty Cycle ####
+	gildi10 = []
+	teljari = 0
+	duty_cycle = 10
+	print("Duty cycle: " + str(duty_cycle) + "%")
+
+	while (teljari < 10):
+		setFanSpeed(duty_cycle)
+		temp_string = read_temp_raw()
+		temp_c = float(temp_string) / 1000.0
+		print(temp_c)
+		gildi10.append(temp_c)
+		time.sleep(2)
+		teljari = teljari + 1
+
+		medaltal10 = sum(gildi10)/10
+		print("Meðaltal 10: " + str(medaltal10))
 
 
-  GPIO.cleanup()
+#Hitastig m.v. 90% duty gildi
+
+	gildi90 = []
+	teljari1 = 0
+	duty_cycle = 90
+	print("Duty cycle: " + str(duty_cycle) + "%")
+
+	while (teljari1 < 10):
+		setFanSpeed(duty_cycle)
+		temp_string = read_temp_raw()
+		temp_c = float(temp_string) / 1000.0
+		print(temp_c)
+		gildi90.append(temp_c)
+		time.sleep(2)
+		teljari1 = teljari1 + 1
+
+	medaltal90 = sum(gildi90)/10
+	print("Meðaltal 90: " + str(medaltal90))
+
+	raunhitastig = (medaltal10 + medaltal90)/2
+	print("Miðgildið er " + str(raunhitastig))
+
+	currenttimi = time.time()
+
+	while (timi <= 30)
+		timi = time.time() - currenttimi
+		k = 3
+		e = oskgildi - temp_c
+		setFanSpeed(k * e)
+		print(e)
+
+		GPIO.cleanup()
+		print("FAN IS OFF")
 
 except KeyboardInterrupt:
-  setFanSpeed(FAN_OFF)
-  GPIO.cleanup()
+	setFanSpeed(FAN_OFF)
+
+	## finna meðaltal af mælingunum tíu
